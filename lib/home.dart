4e2fc557e6b5359b1d3c41a6456e5292c8fbfe61
rensf <2636618553@qq.com>
2019-10-15 15:54:19 +0800

    精灵球制作完毕

--- lib/home.dart
+++ lib/home.dart
@@ -21,13 +21,44 @@ class HomePage extends StatelessWidget {
             border: Border.all(color: Colors.black, width: 8),
             shape: BoxShape.circle,
           ),
-          child: Container(
-            width: 180,
-            height: 85,
-            decoration: BoxDecoration(
-              color: HexColor("#ff0000"),
-              borderRadius: BorderRadius.only(topLeft: Radius.circular(100),topRight: Radius.circular(100))
-            ),
+          child: Stack(
+            children: [
+              Positioned(
+                child: Container(
+                  width: 180,
+                  height: 85,
+                  decoration: BoxDecoration(
+                      color: HexColor("#ff0000"),
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(100),
+                          topRight: Radius.circular(100))
+                  ),
+                ),
+              ),
+              Positioned(
+                child: Container(
+                  width: 180,
+                  height: 0,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 4),
+                  ),
+                ),
+                top: 85,
+              ),
+              Positioned(
+                  child: Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 6),
+                    shape: BoxShape.circle,
+                    color: Colors.white
+                  ),
+                ),
+                top: 65,
+                left: 62,
+              ),
+            ],
           ),
         ),
       ),
